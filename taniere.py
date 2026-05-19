7dcec853dc7b1a252571cc64fd61b61d3648bcf4
01pieV <jamriska.janko267@gmail.com>
2026-05-19 21:12:13 +0200

    Update taniere.py

--- taniere.py
+++ taniere.py
@@ -1,37 +1,41 @@
 import tkinter as tk
 import random
-
-win = tk.Tk()
-canvas = tk.Canvas(win, width=800, height=150, bg = "white")
+window = tk.Tk()
+canvas = tk.Canvas(window,width=800,height=200, bg="white")
 canvas.pack()
-idex = []
-x = 10
-y = 10
-a = ["A","B","C","D","E","F","G","H","I","J"]
+taniere = []
+pismena = ["A","B","C","D","E","F","G","H","I","J"]
 pocet = {}
+x = 10
 for i in range(10):
-    tag = f"tanier{i}"
-    canvas.create_oval(x, y, x+60, y+60, fill="blue", outline="black", width=3, tags=tag)
-    canvas.create_oval(x+10, y+10, x+50, y+50, tags=tag)
-    canvas.create_text(x+30, y+30, text=a[i], fill="white", font=("Arial", 20), tags=tag)
-    idex.append(tag)
+    tag = pismena[i]
+    canvas.create_oval(x,20,x+70,90, fill="red",tags=tag)
+    canvas.create_oval(x+10,30,x+60,80,tags=tag)
+    canvas.create_text(x+35,55,text = pismena[i],fill="white",font=("Arial",20),tags=tag)
+    taniere.append(tag)
     pocet[tag] = 0
     x += 80
 
-vyherny = random.choice(idex)
-print(vyherny)
+vyhra = random.choice(taniere)
+print(vyhra)
+print(taniere)
 def klik(event):
     global pocet
     obj = canvas.find_withtag("current")
     if obj:
-        o = canvas.gettags(obj[0])[0]
-        pocet[o] += 1
-        if o == vyherny:
+        item_id = obj[0]
+        tagy = canvas.gettags(item_id)
+        if tagy[0] != vyhra:
+            pocet[tagy[0]] += 1
+        if tagy[0] == vyhra:
+            viac = []
+            for a, b in pocet.items():
+                if b != 0:
+                    viac.append(a)
             canvas.delete("all")
-            canvas.create_text(400,70,text="vyhral si",fill="red",font=("Arial", 40))
-            print(pocet)
+            canvas.create_text(400, 70, text="vyhral si", fill="blue", font=("Arial", 40))
+            canvas.create_text(400, 120, text=f"viackrát si klikol na taniere: {", ".join(viac)}", fill="red", font=("Arial", 20))
+            print(viac)
 
-
-canvas.bind("<Button-1>", klik)
-win.mainloop()
-
+canvas.bind("<Button-1>",klik)
+window.mainloop()
